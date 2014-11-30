bc637e0f7e406bd99cddca6fdcf0c06d775642dc
StanDimitroff <standimitroff@gmail.com>
2014-11-30 17:48:44 +0200

    song name fixed, download option added

--- upload mp3/scripts/upload.js
+++ upload mp3/scripts/upload.js
@@ -1,252 +1,241 @@
-(function() {
-	$(document).ready(function() {
-		var file;
+(function () {
+    $(document).ready(function () {
+        var file;
+        var headers = {
+            "X-Parse-Application-Id": "LlxgjVpQeDR5hNQwUeurn7FvwDsJ5asIediNz4gS",
+            "X-Parse-REST-API-Key": "hSiN54s0we68AaQaQJCauFXNfE4w8J3nPppcRyPE"
+        };
 
-		$('#fileselect').change(function() {
-			var ext = this.value.match(/\.(.+)$/)[1];
-			switch (ext) {
-				case 'mp3':
-				case 'wav':
-				case 'ogg':
-					$('#uploadbutton').removeAttr('disabled');
-					$('#uploadbutton').click(uploadSong);
-					break;
-				default:
-					$('#uploadbutton').attr('disabled', "disabled");
-					alert('This is not an allowed file type.');
-					this.value = '';
-			}
-		});
+        $('#fileselect').change(function () {
+            var ext = this.value.match(/\.(.+)$/)[1];
+            switch (ext) {
+                case 'mp3':
+                case 'wav':
+                case 'ogg':
+                    $('#uploadbutton').removeAttr('disabled');
+                    $('#uploadbutton').click(function () {
+                        uploadSong(ext)
+                    });
+                    break;
+                default:
+                    $('#uploadbutton').attr('disabled', "disabled");
+                    alert('This is not an allowed file type.');
+                    this.value = '';
+            }
+        });
 
-		$('#btn-upload').click(function() {
-			$('#fileselect').click();
-			// Set an event listener on the Choose File field.
-			$('#fileselect').bind("change", function(e) {
-				files = e.target.files || e.dataTransfer.files;
-				// Our file var now holds the selected file
-				file = files[0];
+        $('#btn-upload').click(function () {
+            $('#fileselect').click();
+            // Set an event listener on the Choose File field.
+            $('#fileselect').bind("change", function (e) {
+                files = e.target.files || e.dataTransfer.files;
+                // Our file var now holds the selected file
+                file = files[0];
 
-			});
-		});
+            });
+        });
 
+        loadSongs();
 
+        function loadSongs() {
+            $.ajax({
+                method: 'GET',
+                headers: headers,
+                // "X-Parse-Master-Key" : "ymbyUxlC9smIS2c5jfYQFjC9tThumZYWHPlBq9nE"
 
-		var PARSE_APP_ID = "LlxgjVpQeDR5hNQwUeurn7FvwDsJ5asIediNz4gS";
-		var PARSE_API_KEY = "hSiN54s0we68AaQaQJCauFXNfE4w8J3nPppcRyPE";
+                url: "https://api.parse.com/1/classes/SongsStorage",
+                success: songsLoaded,
+                error: songsLoadedError
+            });
+        }
 
-		loadSongs();
+        function songsLoaded(data) {
+            $('#uploadbutton').attr('disabled', "disabled");
+            $('#showSongs').html("");
+            for (var s in data.results) {
+                var songObj = data.results[s]['song'];
+                var song = data.results[s];
+                var songUrl = songObj.url;
+                var songName = data.results[s].songName;
+                var div = $('<div>').addClass("dipslayedSongs");
+                var link = $('<a>').attr("href", songUrl);
+                var $delete = $('<button>').addClass("delete-btn").text("Delete");
+                $delete.data('song', song);
+                $delete.appendTo(div);
+                $delete.click(deleteSong);
+                var download = $('<a id="dw-btn">').attr('href',songUrl).attr('download', songName).text('Download');
+                download.appendTo(div);
+                link.text(songName);
+                link.appendTo(div);
+                div.appendTo($('#showSongs'));
+            }
+        }
 
-		function loadSongs() {
-			$.ajax({
-				method: 'GET',
-				headers: {
-					"X-Parse-Application-Id": PARSE_APP_ID,
-					"X-Parse-REST-API-Key": PARSE_API_KEY
-						// "X-Parse-Master-Key" : "ymbyUxlC9smIS2c5jfYQFjC9tThumZYWHPlBq9nE"
-				},
-				url: "https://api.parse.com/1/classes/SongsStorage",
-				success: songsLoaded,
-				error: songsLoadedError
-			});
-		}
+        function uploadSong(ext) {
+            $('#prog').progressbar({
+                value: 0
+            });
+            var songGenre = $('#songGenre option:selected').text();
+            // This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
+            var songName = file.name.substring(0, file.name.indexOf(ext) - 1);
 
-		function songsLoaded(data) {
-			$('#uploadbutton').attr('disabled', "disabled");
-			$('#showSongs').html("");
-			for (var s in data.results) {
-				var songObj = data.results[s]['song'];
-				var song = data.results[s];
-				var songUrl = songObj.url;
-				var songName = data.results[s].songName;
-				var div = $('<div>').addClass("dipslayedSongs");
-				var link = $('<a>').attr("href", songUrl);
-				var $delete = $('<button>').addClass("delete-btn").text("Delete");
-				$delete.data('song', song);
-				$delete.appendTo(div);
-				$delete.click(deleteSong);
-				link.text(songName);
-				link.appendTo(div);
-				div.appendTo($('#showSongs'));
+            console.log(songName);
+            var serverUrl = 'https://api.parse.com/1/files/' + songName;
 
-			}
-		}
+            $.ajax({
+                type: "POST",
+                headers: headers,
+                "Content-Type": file.type,
 
-		function uploadSong() {
-			$('#prog').progressbar({
-				value: 0
-			});
-			var songGenre = $('#songGenre option:selected').text();
-			// This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
-			var songName = file.name.match(/(.+).\./)[1];
-			console.log(songName);
-			var serverUrl = 'https://api.parse.com/1/files/' + songName;
+                url: serverUrl,
+                data: file,
+                processData: false,
+                contentType: false,
+                success: function (data) {
 
-			$.ajax({
-				type: "POST",
-				headers: {
-					"X-Parse-Application-Id": PARSE_APP_ID,
-					"X-Parse-REST-API-Key": PARSE_API_KEY,
-					"Content-Type": file.type
-				},
-				url: serverUrl,
-				data: file,
-				processData: false,
-				contentType: false,
-				success: function(data) {
+                    alert("File available at: " + data.url);
+                    writeSongToSongsStorage(data, songGenre, songName);
+                },
+                error: function (data) {
+                    var obj = jQuery.parseJSON(data);
+                    alert(obj.error);
+                },
+                progress: function (e) {
+                    if (e.lengthComputable) {
+                        var pct = (e.loaded / e.total) * 100;
 
-					alert("File available at: " + data.url);
-					writeSongToSongsStorage(data, songGenre, songName);
-				},
-				error: function(data) {
-					var obj = jQuery.parseJSON(data);
-					alert(obj.error);
-				},
-				progress: function(e) {
-					if (e.lengthComputable) {
-						var pct = (e.loaded / e.total) * 100;
+                        $('#prog')
+                            .progressbar('option', 'value', pct)
+                            .children('.ui-progressbar-value')
+                            .html(pct.toPrecision(3) + '%')
+                            .css('display', 'block');
+                    } else {
+                        console.warn('Content Length not reported!');
+                    }
+                }
 
-						$('#prog')
-							.progressbar('option', 'value', pct)
-							.children('.ui-progressbar-value')
-							.html(pct.toPrecision(3) + '%')
-							.css('display', 'block');
-					} else {
-						console.warn('Content Length not reported!');
-					}
-				}
+            });
+        }
 
-			});
+        function writeSongToSongsStorage(data, songGenre, songName) {
+            $('#progress-wrapper').html("");
+            $("<div>").attr('id', 'prog').appendTo($("#progress-wrapper"));
+            var songUrl = data.url;
+            $.ajax({
+                method: 'POST',
+                headers: headers,
+                url: "https://api.parse.com/1/classes/SongsStorage",
+                data: JSON.stringify({
+                    "songName": songName,
+                    "songGenre": songGenre,
+                    "songFileName": data.name,
+                    "forDelete": false,
+                    "song": {
+                        "__type": "File",
+                        "name": data.name,
+                        "url": songUrl
+                    }
+                }),
+                contentType: "application/json",
+                success: loadSongs,
+                error: songsLoadedError
+            });
+        }
 
-		}
+        function deleteSong() {
+            var song = $(this).data('song');
+            $.ajax({
+                method: 'PUT',
+                url: "https://api.parse.com/1/classes/SongsStorage/" + song.objectId,
+                data: JSON.stringify({
+                    "forDelete": true
+                }),
 
-		function writeSongToSongsStorage(data, songGenre, songName) {
-			$('#progress-wrapper').html("");
-			$("<div>").attr('id', 'prog').appendTo($("#progress-wrapper"));
-			var songUrl = data.url;
-			$.ajax({
-				method: 'POST',
-				headers: {
-					"X-Parse-Application-Id": PARSE_APP_ID,
-					"X-Parse-REST-API-Key": PARSE_API_KEY
-				},
-				url: "https://api.parse.com/1/classes/SongsStorage",
-				data: JSON.stringify({
-					"songName": songName,
-					"songGenre": songGenre,
-					"songFileName" : data.name,
-					"forDelete": false,
-					"song": {
-						"__type": "File",
-						"name": data.name,
-						"url": songUrl
-					}
-				}),
-				contentType: "application/json",
-				success: loadSongs,
-				error: songsLoadedError
-			});
-		}
+                contentType: "application/json",
+                success: function (data) {
+                    callParseCloudCodeForDelete();
+                    deleteSongObject(song);
+                    deleteFileSuccessfully();
 
-		function deleteSong() {
-			var song = $(this).data('song');
-			$.ajax({
-				method: 'PUT',
-				headers: {
-					"X-Parse-Application-Id": PARSE_APP_ID,
-					"X-Parse-REST-API-Key": PARSE_API_KEY
-				},
-				url: "https://api.parse.com/1/classes/SongsStorage/" + song.objectId,
-				data: JSON.stringify({
-					"forDelete": true
-				}),
+                },
 
-				contentType: "application/json",
-				success: function(data) {
-					callParseCloudCodeForDelete();
-					deleteSongObject(song);
-					deleteFileSuccessfully();
-					
-				},
+                error: songsLoadedError
+            });
+        }
 
-				error: songsLoadedError
-			});
-		}
+        function deleteFileSuccessfully() {
+            alert("deleteFileSuccessfully");
+        }
 
-		function deleteFileSuccessfully() {
-			alert("deleteFileSuccessfully");
-		}
+        function deleteObject(songId) {
+            console.log(songId);
+        }
 
-		function deleteObject(songId) {
-			console.log(songId);
-		}
+        function songUploaded() {
+            console.log("uploaded");
+        }
 
-		function songUploaded() {
-			console.log("uploaded");
-		}
+        function songsUploadedError() {
+            console.log("Error");
+        }
 
-		function songsUploadedError() {
-			console.log("Error");
-		}
+        function songsLoadedError(e) {
+            console.log(e);
+        }
 
-		function songsLoadedError(e) {
-			console.log(e);
-		}
-		function callParseCloudCodeForDelete () {
-			$.ajax({
-				method: "POST",
-				headers: {
-					"X-Parse-Application-Id": "LlxgjVpQeDR5hNQwUeurn7FvwDsJ5asIediNz4gS",
-					"X-Parse-REST-API-Key": "hSiN54s0we68AaQaQJCauFXNfE4w8J3nPppcRyPE"
-				},
-				url: "https://api.parse.com/1/functions/delete",
-				success: function(data) {
-					console.log(data);
-				},
-				error: function() {
-					console.log("not OK");
-				}
-			});
-		}
+        function callParseCloudCodeForDelete() {
+            $.ajax({
+                method: "POST",
+                headers: {
+                    "X-Parse-Application-Id": "LlxgjVpQeDR5hNQwUeurn7FvwDsJ5asIediNz4gS",
+                    "X-Parse-REST-API-Key": "hSiN54s0we68AaQaQJCauFXNfE4w8J3nPppcRyPE"
+                },
+                url: "https://api.parse.com/1/functions/delete",
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function () {
+                    console.log("not OK");
+                }
+            });
+        }
 
-		function deleteSongObject (song) {
-		
-			console.log(song);
-			$.ajax({
+        function deleteSongObject(song) {
 
-				method: "DELETE",
-				headers: {
-					"X-Parse-Application-Id": "LlxgjVpQeDR5hNQwUeurn7FvwDsJ5asIediNz4gS",
-					"X-Parse-REST-API-Key": "hSiN54s0we68AaQaQJCauFXNfE4w8J3nPppcRyPE"
-				},
-				url: "https://api.parse.com/1/classes/SongsStorage/" + song.objectId,
-				success: function(data) {
-					console.log(data);
-					loadSongs();
-				},
-				error: function() {
-					console.log("not OK");
-				}
-			});
+            console.log(song);
+            $.ajax({
 
-		}
-		$("#test").click(function() {
-			$.ajax({
+                method: "DELETE",
+                headers: {
+                    "X-Parse-Application-Id": "LlxgjVpQeDR5hNQwUeurn7FvwDsJ5asIediNz4gS",
+                    "X-Parse-REST-API-Key": "hSiN54s0we68AaQaQJCauFXNfE4w8J3nPppcRyPE"
+                },
+                url: "https://api.parse.com/1/classes/SongsStorage/" + song.objectId,
+                success: function (data) {
+                    console.log(data);
+                    loadSongs();
+                },
+                error: function () {
+                    console.log("not OK");
+                }
+            });
+        }
 
-				method: "POST",
-				headers: {
-					"X-Parse-Application-Id": "LlxgjVpQeDR5hNQwUeurn7FvwDsJ5asIediNz4gS",
-					"X-Parse-REST-API-Key": "hSiN54s0we68AaQaQJCauFXNfE4w8J3nPppcRyPE"
-				},
-				url: "https://api.parse.com/1/functions/show",
-				success: function(data) {
-					console.log(data);
-				},
-				error: function() {
-					console.log("not OK");
-				}
-			});
-		});
-	});
+        $("#test").click(function () {
+            $.ajax({
+
+                method: "POST",
+                headers: headers,
+                url: "https://api.parse.com/1/functions/show",
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function () {
+                    console.log("not OK");
+                }
+            });
+        });
+    });
 }());
 
 // delete file - https://api.parse.com/1/files/ + file.name
